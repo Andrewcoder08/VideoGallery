aac911042eaeacefdbba5628f0dfb5c48ce4ac21
Andrewcoder08 <andrewcoder08@gmail.com>
2023-07-21 21:17:53 +0530

    Fix: commented JS as not needed in iframe approach

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const videos = document.querySelectorAll(".video-container video");
-videos.forEach((videoSelected) => {
-  videoSelected.addEventListener("click", function () {
-    // adding class that is responsible for making the video focused in center
-    videoSelected.classList.toggle("active");
-    // paused is in built property of video
-    if (videoSelected.paused) {
-      videoSelected.play();
-    } else {
-      videoSelected.pause();
-      // start video from beginning
-      videoSelected.currentTime = 0;
-    }
-  });
-});
+// this is needed if we do for video but I have used iframe
+
+// const videos = document.querySelectorAll(".video-container video");
+// videos.forEach((videoSelected) => {
+//   videoSelected.addEventListener("click", function () {
+//     // adding class that is responsible for making the video focused in center
+//     videoSelected.classList.toggle("active");
+//     // paused is in built property of video
+//     if (videoSelected.paused) {
+//       videoSelected.play();
+//     } else {
+//       videoSelected.pause();
+//       // start video from beginning
+//       videoSelected.currentTime = 0;
+//     }
+//   });
+// });
